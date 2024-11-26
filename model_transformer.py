f92e76e8096c350eca9f6ede10a9116ce12048b9
takutosugawara <tatakuku0724@gmail.com>
2024-11-26 13:11:17 +0900

    transformer版を追加　後で統合

---
+++ model_transformer.py
@@ -0,0 +1,26 @@
+import torch
+import music21
+import numpy as np
+import os 
+import settings
+
+
+
+
+
+
+
+
+def write_output_midi():
+    #outputMIDIファイルを適切なファイル名で保存する
+    pass
+
+
+def main():
+    try:
+        print("hogehoge")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+if __name__ == '__main__':
+    main()
