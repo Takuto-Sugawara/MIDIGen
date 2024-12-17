a64f33a706601bf1a13571e7d7890ad12034c1c7
takutosugawara <tatakuku0724@gmail.com>
2024-12-17 15:27:19 +0900

    add

--- model_transformer.py
+++ model_transformer.py
@@ -1,22 +1,42 @@
 import torch
 import torch.nn as nn
-import music21
+import mido
 import numpy as np
 import os 
 import settings
 
+#とりあえずLSTMで実装してみる。後でTransformerに変更する！！！！！！！！！
+class MIDIgen(nn.Module):
+    def __init__(self, sequence_length):
+        super().__init__()
+        self.sequence_length = sequence_length
+        self.device = grab_divice_type()
+        self.flatten = nn.Flatten()
+        self.LSTM = nn.LSTM(input_size=4, hidden_size=256, num_layers=1, batch_first=True)
+        #input_size: 入力の特徴量の数(dim: pitch, velocity, duration, time)
+        self.dropout = nn.Dropout(dropout_rate=0.1)
+        self.linear = nn.Linear(128, 128)
+        self.ReLU = nn.ReLU()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
 
-def grab_divice_type():
-    #GPUが利用可能ならGPUを利用する
-    pass
+    def forward(self, x):
+        pass
+
+    def backward(self, x):
+        pass
 
 
 
 
 
-
-
-
+def grab_divice_type(enable_gpu=True):
+    if torch.cuda.is_available() and enable_gpu:
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available() and enable_gpu:
+        device = torch.device("cpu")
+    else:
+        device = torch.device("cpu")
+    return device
 
 
 def write_output_midi():
